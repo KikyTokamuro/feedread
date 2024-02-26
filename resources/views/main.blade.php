b74eed8e17644de11ec48c4193df479742a1c1bd
KikyTokamuro <kiky.tokamuro@yandex.ru>
2024-02-26 17:38:06 +0300

    Update main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="d-flex align-items-center justify-content-center" style="height: 250px;">
-        <i class="fs-1 bi bi-rss-fill"></i>
-        <h1 class="display-4">FeedRead</h1>
+    <div class="d-flex flex-column align-items-center justify-content-center" style="height: 300px;">
+        <h1><i class="fs-1 bi bi-rss-fill"></i>FeedRead</h1>
+        <div class="lead">Simple Atom/RSS reader</div>
+        <a href="https://github.com/KikyTokamuro/feedread" class="mt-3 text-decoration-none" target="_blank">
+            <i class="bi bi-github"></i> Source
+        </a>
     </div>
+
 @endsection
