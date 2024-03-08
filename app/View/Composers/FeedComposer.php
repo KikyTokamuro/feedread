3a12686d1c3d5b8a4678f1f70f9b0bbc5b73f012
Daniil Arkhangelsky <kiky.tokamuro@yandex.ru>
2024-03-08 21:08:31 +0300

    Updated feeds order

--- app/View/Composers/FeedComposer.php
+++ app/View/Composers/FeedComposer.php
@@ -12,6 +12,6 @@ class FeedComposer
      */
     public function compose(View $view)
     {
-        $view->with('feeds', Feed::all());
+        $view->with('feeds', Feed::orderBy('created_at', 'desc')->get());
     }
 }
