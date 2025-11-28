3fd89f126cb3e13c2f1b6897f67470f5ed7c65a6
Daniil Arkhangelsky <kiky.tokamuro@yandex.ru>
2025-11-28 09:45:00 +0300

    Added logo in sidebar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="d-flex flex-column align-items-center justify-content-center" style="height: 80%;">
-        <h1><i class="fs-1 bi bi-rss-fill"></i>FeedRead</h1>
+        <h1><i class="text-warning fs-1 bi bi-rss-fill"></i>FeedRead</h1>
         <div class="lead">Simple Atom/RSS reader</div>
         <a href="https://github.com/KikyTokamuro/feedread" class="mt-3 text-decoration-none" target="_blank">
             <i class="bi bi-github"></i> Source
